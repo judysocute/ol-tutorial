04a5028585857aabc3b69e14def545d3d5e07557
WayneChangCA <wayne.chang@ibuypower.com>
2025-10-21 17:06:58 +0800

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import MyMap from "./components/MyMap";
+import MyMap from "@/components/MyMap";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <MyMap /> {/* default projection EPSG:3857 */}
       {/* <MyMap projection="ESRI:54030" /> */}
+      <Button>hihi</Button>
     </div>
   );
 }
