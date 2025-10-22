094eb2df92c462ca6f568a13596638db831bc67f
WayneChangCA <wayne.chang@ibuypower.com>
2025-10-22 17:01:41 +0800

    add shadcn sidebar

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -1,20 +1,77 @@
+import { Calendar, Home, Inbox, Search, Settings, ScanEye } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
   SidebarHeader,
-} from "@/components/ui/sidebar"
- 
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const items = [
+  {
+    title: "Zoom Slider",
+    turnOn: false,
+    icon: ScanEye,
+  },
+  {
+    title: "Inbox",
+    turnOn: false,
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    turnOn: false,
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    turnOn: false,
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    turnOn: false,
+    icon: Settings,
+  },
+];
+
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader>Map</SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Function Display</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
-  )
+  );
 }
